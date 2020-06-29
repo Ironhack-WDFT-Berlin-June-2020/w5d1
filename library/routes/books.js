ddbacccc91bdea413b1d9cfd0afefd3cc20993d2
Jan Redmann <janredmann1@gmail.com>
2020-06-29 17:47:36 +0200

    add library crud

--- library/routes/books.js
+++ library/routes/books.js
@@ -13,6 +13,10 @@ router.get('/books', (req, res) => {
   })
 });
 
+router.get('/books/add', (req, res) => {
+  res.render('bookForm');
+})
+
 router.get('/books/:bookId', (req, res) => {
   const bookId = req.params.bookId;
   Book.findById(bookId).then(bookFromDatabase => {
@@ -21,5 +25,61 @@ router.get('/books/:bookId', (req, res) => {
     console.log(err);
   });
 });
+
+router.post('/books', (req, res) => {
+  console.log(req.body);
+  // const title = req.body.title;
+  // const author = req.body.author;
+  // const description = req.body.description;
+  // const rating = req.body.rating;
+  const { title, author, description, rating } = req.body;
+  Book.create({
+    title: title,
+    author: author,
+    description: description,
+    rating: rating
+  }).then(book => {
+    console.log(`Success! ${title} was added to the database.`);
+    res.redirect(`/books/${book._id}`);
+  }).catch(err => {
+    console.log(err);
+  })
+})
+
+router.get('/books/edit/:bookId', (req, res) => {
+  Book.findById(req.params.bookId)
+    .then(book => {
+      res.render('bookEdit', { book: book })
+    }).catch(err => {
+      console.log(err);
+    });
+})
+
+router.get('/books/delete/:bookId', (req, res) => {
+  Book.deleteOne({ _id: req.params.bookId })
+    .then(() => {
+      res.redirect('/books');
+    })
+    .catch(err => {
+      console.log(err);
+      // next(err)
+    })
+})
+
+router.post('/books/edit/:bookId', (req, res) => {
+  const { title, author, description, rating } = req.body;
+  Book.findByIdAndUpdate(req.params.bookId, {
+    title,
+    description,
+    author,
+    rating
+  })
+    .then(book => {
+      res.redirect(`/books/${book._id}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
 
 module.exports = router;
